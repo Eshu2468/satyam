9087cfb6eec506d745024299d14c96c787f2ce4e
eshu2468 <eshugowda2000@gmail.com>
2025-04-26 21:16:44 +0530

    second changes in file vishwa

--- vishwa.py
+++ vishwa.py
@@ -3,3 +3,10 @@ from day7 import add, sub, modulus, multi, divide, floorediv
 
 print(add(10000,5000))
 print(modulus(200,50))
+
+
+print("hello world")
+print("git")
+print("github")
+variab = 123445 + 1345678
+print(variab)
